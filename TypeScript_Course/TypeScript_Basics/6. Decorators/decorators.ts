e3aa94792f44e056831cee178489a5243f3e4410
Harsh Siddhapura <harsh.siddhapura@indexnine.com>
2022-01-02 19:01:24 +0530

    Updated the Decorators.ts file

--- TypeScript_Course/TypeScript_Basics/6. Decorators/decorators.ts
+++ TypeScript_Course/TypeScript_Basics/6. Decorators/decorators.ts
@@ -44,3 +44,89 @@ class Persons{
 
 const per = new Persons();
 console.log(per);
+
+
+// Property Decorators =================================================
+console.log('\n\nProperty Decorators...!!!');
+
+function Log1(target: any, propertyName: string)
+{
+    console.log("Property Decorator..");
+    console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor)
+{
+    console.log('\nAccessor Decorator..');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log3(target: any, name: string|Symbol, descriptor: PropertyDescriptor)
+{
+    console.log('\nMethod Decorator..');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+class Product{
+    @Log1
+    title: string;
+    private _price: number;
+
+    @Log2
+    set price(val: number)
+    {
+        if(val>0) this._price = val;
+        else throw new Error('Price should be Positive...');
+    }
+
+    constructor(t: string, p: number)
+    {
+        this.title = t;
+        this._price = p;
+    }
+
+    @Log3
+    priceWithTax(tax: number)
+    {
+        return this._price * (1+tax);
+    }
+}
+
+
+
+// Autobind Decorator Example with Button =================================================
+console.log('\n\nAutobind Decorator Example with Button...!!!');
+
+function Autobind(_1: any, _2: string, descriptor: PropertyDescriptor)
+{
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get()
+        {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+class Printer{
+    message = 'Button works with Autobind Decorator';
+
+    @Autobind
+    showMessage()
+    {
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
+
